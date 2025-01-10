db553659387649c8d2184e1f79b65fc4735b6d14
Abhishek Kumar <125857281+abhishekchaurasia0707@users.noreply.github.com>
2025-01-10 12:10:53 +0000

    datatype in javascript

---
+++ 01_basic/datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treal all JS code as newer version
+// alert(3+3) //we are using nodejs,not browser so this thing will give error
+
+
+
+//Datatypes in Javascript
+ let nam="abhishek" // string
+ let num=12 // integer
+ let isLoggedIn= true // boolean
+
+
+
+//===================================primitve datatypes========================================
+ // number => 2 to power 53
+ //bigint
+ // string=""
+ // boolean => true/false
+ // null => standlone value
+ // undefined => means value not assigned
+ // symbol => used for uniqueness
+
+ //object
+
+ console.log(typeof nam)
+ console.log(typeof num)
+ console.log(typeof null)// will give object as output
+ console.log(typeof undefined)// will give undefined
+ console.table([typeof nam,typeof num,typeof null,typeof undefined])
